99ef099041969314d47d7d15fbabcdfaa6ffb424
NageshPolu <nagesh.polu@outlook.com>
2025-12-15 16:24:47 +0100

    Refactor EmpJob API call to simplify fields
    Removed filtering and selection of job fields from the API call.

--- gates.py
+++ gates.py
@@ -22,24 +22,4 @@ def run_ec_gates(sf) -> dict:
 
     # EMPJOB (your tenant confirmed fields)
     jobs = sf.get_all("/odata/v2/EmpJob", {
-        "$select": "userId,managerId,company,businessUnit,division,department,location,effectiveLatestChange",
-        "$filter": "effectiveLatestChange eq true",
-    })
-
-    ORG_FIELDS = ["company", "businessUnit", "division", "department", "location"]
-    missing_manager = 0
-    invalid_org = 0
-
-    # Drilldown samples (keep small to avoid huge payloads)
-    MAX_SAMPLE = 200
-    invalid_org_rows = []
-    missing_manager_rows = []
-    org_missing_counts = {k: 0 for k in ORG_FIELDS}
-
-    for j in jobs:
-        uid = j.get("userId")
-        mgr = j.get("managerId")
-
-        # Missing manager
-        if is_blank(mgr):
-            missing
+        "$select": "userId,managerId,company,businessU
